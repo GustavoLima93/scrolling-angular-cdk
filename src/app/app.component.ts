fe9521296fad688d20550decf16002db69f3099c
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2019-01-26 12:59:10 -0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  title = 'scrolling';
+
+  items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
+}
